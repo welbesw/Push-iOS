2828c05cc4d2a575737913a5430fabaa96bc795c
William Welbes <williamj@gmail.com>
2019-04-05 14:23:14 -0500

    Updated to swift 5.

--- Push/AppDelegate.swift
+++ Push/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         //Assign the push delegate before didFinishLaunchingWithOptions completes
         UNUserNotificationCenter.current().delegate = PushManager.sharedInstance
